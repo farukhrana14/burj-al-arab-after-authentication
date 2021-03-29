27ad38821432f714f3ce58b913fa82ce6285897f
Farukhrana14 <farukhrana14@gmail.com>
2021-03-29 08:52:21 +0600

    config deleted

--- configs/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCAqPQgNHSXAFYtwmnltbd4GkHwoDs6zrM",
-  authDomain: "burj-al-arab-1d1c3.firebaseapp.com",
-  projectId: "burj-al-arab-1d1c3",
-  storageBucket: "burj-al-arab-1d1c3.appspot.com",
-  messagingSenderId: "642291159425",
-  appId: "1:642291159425:web:0b0a7bc77f552da0261880"
-  };
-  export default firebaseConfig;
